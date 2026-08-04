03d8566aecfff56c41eb880ebce6070fc740f137
Priyanka <priyankas302005@gmail.com>
2026-08-04 19:45:24 +0530

    Completed User Management CRUD APIs

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: str
+    role: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    full_name: str
+    email: str
+    role: str
+
+    class Config:
+        from_attributes = True
