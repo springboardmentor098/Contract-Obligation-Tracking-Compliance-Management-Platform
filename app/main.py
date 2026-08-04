b3f31939188244d72f49a8b110e69e718377c361
Chanda-Sravani-27 <chanda.sravani27@gmail.com>
2026-08-04 20:15:51 +0530

    Completed User Management CRUD operations

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-
-from app.api.user_api import router as user_router
+from app.routers.users import router as user_router
 from app.database.database import test_database_connection
 
 app = FastAPI(
