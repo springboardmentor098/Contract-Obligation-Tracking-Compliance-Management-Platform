c8f00095a6abf531279512fbca56297d8c073f70
Aniket Verma <aniketbtech15@gmail.com>
2026-08-05 23:16:45 +0530

    Add backend implementation

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from app.database.database import test_database_connection
+
+
+app = FastAPI(
+    title="ContractIQ API",
+    version="1.0.0"
+)
+
+
+@app.on_event("startup")
+def startup_event():
+    test_database_connection()
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "ContractIQ Backend is running successfully."
+    }         
